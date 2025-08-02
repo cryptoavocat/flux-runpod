08a82f3c2fdc4c15bf640548e3216b89c6a2700f
legalabs <109113798+cryptoavocat@users.noreply.github.com>
2025-08-02 21:16:11 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -19,7 +19,7 @@ print("✅ Model loaded to CUDA.")
 # Main RunPod handler function
 def handler(event):
     try:
-        prompt = event["input"].get("prompt", "Astronaut on an island on a deserted planet with a cocktail in hand")
+        prompt = event["input"].get("prompt", "Astronaut on a deserted planet looking at earth in the sky being destroyed, with a dirty martini in hand")
         print(f"🚀 Prompt received: {prompt}")
         
         image = pipe(prompt).images[0]
